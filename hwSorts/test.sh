3bf673fad7b4facb0559920f4672c4a19abccdd6
Stephan Doan <sdoan@stuy.edu>
2016-01-06 23:52:09 -0500

    add test shell

---
+++ hwSorts/test.sh
@@ -0,0 +1,29 @@
+
+echo ----------------
+echo -Insertion
+echo ---insertion random
+java Driver2 insertion 20000 random
+java Driver2 insertion 40000 random
+java Driver2 insertion 80000 random
+echo ---insertion sorted
+java Driver2 insertion 20000 sorted
+java Driver2 insertion 40000 sorted
+java Driver2 insertion 80000 sorted
+echo ---insertion reversed
+java Driver2 insertion 20000 reversed
+java Driver2 insertion 40000 reversed
+java Driver2 insertion 80000 reversed
+echo ----------------
+echo -Selection
+echo ---selection random
+java Driver2 selection 20000 random
+java Driver2 selection 40000 random
+java Driver2 selection 80000 random
+echo ---selection sorted
+java Driver2 selection 20000 sorted
+java Driver2 selection 40000 sorted
+java Driver2 selection 80000 sorted
+echo ---selection reversed
+java Driver2 selection 20000 reversed
+java Driver2 selection 40000 reversed
+java Driver2 selection 80000 reversed
